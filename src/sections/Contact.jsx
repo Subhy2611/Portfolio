ee0d45c7a6b910974c7fca0f25e2aefda4c8a8b4
Subhy2611 <isubhamghosh26@gmail.com>
2025-09-12 17:29:56 +0530

    Path update

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import TitleHeader from "../components/TitleHeader";
-import ContactExperience from "../components/models/Contact/ContactExperience";
+import ContactExperience from "../components/models/Contact/ContactExperience.jsx";
 import emailjs from "@emailjs/browser";
 
 const Contact = () => {
